115b47301621bdd57e6cccf95f92a41ff3a6f078
Kayvan Arianpour <karianpour@gmail.com>
2019-03-24 23:25:52 +0430

    adding types

---
+++ src/lib/types/index.d.ts
@@ -0,0 +1,5 @@
+import DateTimeInputSimple from "./DateTimeInput";
+import DateInputSimple from "./DateInput";
+import DateInput from "./DateInput";
+import DateTimeInput from "./DateTimeInput";
+export { DateTimeInput, DateInput, DateInputSimple, DateTimeInputSimple };
